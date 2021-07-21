6c69668c53f2c871ed59fe0d8178a3b76eb1a2be
ximelh <ximenaloarca@gmail.com>
2021-07-21 12:21:11 -0600

    Actores
    Se agregaron los actores al escenario.

---
+++ LaberintoGreenfoot/Arándano.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Arándano here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Arándano extends Actor
+{
+    /**
+     * Act - do whatever the Arándano wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
